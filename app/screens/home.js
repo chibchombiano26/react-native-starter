7fb92744ec6cfa650449cdf2c39744e4a70f0c21
Jose douglas ramirez e <futbolito152@gmail.com>
2018-07-28 19:32:43 -0500

    eslint

--- app/screens/home.js
+++ app/screens/home.js
@@ -1,37 +1,49 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import {
+  View, Text, StyleSheet, Button,
+} from 'react-native';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as HelloActions from '../../redux/actions/Hello';
 
-class Home extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>{this.props.data.helloName}</Text>
-                <Button title="Change text" onPress={()=>{
-                    this.props.helloActions.sayHelloAction("New Name");
-                }} />
-            </View>
-        )
-    }
-}
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
 
-const mapStateToProps = (state, props) =>({
-    data: state.Hello
-})
+class Home extends Component {
+  componentDidMount() {
 
-const mapDistpachToProps = (distpatch) =>  ({
-   helloActions: bindActionCreators(HelloActions, distpatch)
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          {this.props.data.helloName}
+        </Text>
+        <Button
+          title="Change text"
+          onPress={() => {
+            this.props.helloActions.sayHelloAction('New Name');
+          }}
+        />
+      </View>
+    );
+  }
+}
+
+
+const mapStateToProps = state => ({
+  data: state.Hello,
+});
+
+const mapDistpachToProps = distpatch => ({
+  helloActions: bindActionCreators(HelloActions, distpatch),
 });
 
 export default connect(mapStateToProps, mapDistpachToProps)(Home);
--- redux/actions/Hello.js
+++ redux/actions/Hello.js
@@ -1,8 +1,8 @@
 import * as actions from '../const';
 
-export const doSayHelloAction = (name)  => (distpach) => distpach(sayHelloAction(name));
+export const sayHelloAction = name => ({
+  type: actions.HELLO,
+  value: name,
+});
 
-export const sayHelloAction = (name) =>({
-    type: actions.HELLO,
-    value: name
-})
+export const doSayHelloAction = name => distpach => distpach(sayHelloAction(name));
